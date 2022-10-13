c743986a5c249a0498eecf711e6b930fae7f1c00
kramstyles <mark.eke@decagon.dev>
2022-10-13 13:22:04 +0100

    added booklist and css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./index.css";
+
 const BookList = () => (
   <section style={{ fontFamily: "poppins" }}>
+    <Book />
     <Book />
   </section>
 );
 
-const Book = () => <article>This is a book</article>;
+const Book = () => (
+  <article>
+    <Image />
+    <Title />
+    <Author />
+  </article>
+);
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/51znoqd9roL._AC_SX184_.jpg"
+    alt="Diary of a wimpy kid"
+  />
+);
+const Title = () => <h4>Diary of a Wimpy Kid</h4>;
+const Author = () => <p>Jeff Kinney</p>;
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
