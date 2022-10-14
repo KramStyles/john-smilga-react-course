8f437efc082c489ba975028815363d461170798f
kramstyles <mark.eke@decagon.dev>
2022-10-14 23:18:28 +0100

    introduced props

--- src/index.js
+++ src/index.js
@@ -3,33 +3,36 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
+const first = {
+  image:
+    "https://images-na.ssl-images-amazon.com/images/I/51znoqd9roL._AC_SX184_.jpg",
+  title: "Diary of a Wimpy Kid",
+  author: "Jeff Kinney",
+};
+
+const second = {
+  image:
+    "https://images-na.ssl-images-amazon.com/images/I/51znoqd9roL._AC_SX184_.jpg",
+  title: "Cash Money Focus",
+  author: "Random Guy",
+};
+
 const BookList = () => (
   <section className="booklist">
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
+    <Book image={first.image} title={first.title} author={first.author} />
+    <Book image={second.image} title={second.title} author={second.author} />
   </section>
 );
 
-const Book = () => (
+const Book = (props) => (
   <article className="book">
-    <Image />
-    <Title />
-    <Author />
+    <Image source={props.image} title={props.title} />
+    <Title title={props.title} />
+    <Author name={props.author} />
   </article>
 );
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/51znoqd9roL._AC_SX184_.jpg"
-    alt="Diary of a wimpy kid"
-  />
-);
-const Title = () => <h4>Diary of a Wimpy Kid</h4>;
-const Author = () => <p>Jeff Kinney</p>;
+const Image = ({ source, title }) => <img src={source} alt={title} />;
+const Title = ({ title }) => <h4>{title}</h4>;
+const Author = ({ name }) => <p>{name}</p>;
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
