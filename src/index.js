3a4bbb374d39e78ecaf46d2705d97ac88e5410e5
kramstyles <mark.eke@decagon.dev>
2022-10-15 11:02:34 +0100

    introduced children

--- src/index.js
+++ src/index.js
@@ -19,18 +19,30 @@ const second = {
 
 const BookList = () => (
   <section className="booklist">
-    <Book image={first.image} title={first.title} author={first.author} />
-    <Book image={second.image} title={second.title} author={second.author} />
+    <Book image={first.image} title={first.title} author={first.author}>
+      <p className="desc">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sit ab
+        fugiat voluptatibus beatae vitae, molestias ipsum repellat mollitia
+        itaque quaerat ex fugit nostrum impedit.
+      </p>
+    </Book>
+    <Book image={second.image} title={second.title} author={second.author}>
+      <p className="desc">
+        To make them have different height, go to book class and use
+        align-items: start
+      </p>
+    </Book>
   </section>
 );
 
 const Book = (props) => {
-  const { image, title, author } = props; // Destructuring to prevent using props each time
+  const { image, title, author, children } = props; // Destructuring to prevent using props each time
   return (
     <article className="book">
       <Image source={image} title={title} />
       <Title title={title} />
       <Author name={author} />
+      {children}
     </article>
   );
 };
