7e8a32ce41674e92e98997dae952c8acfb8f00ff
kramstyles <mark.eke@decagon.dev>
2022-10-17 23:21:16 +0100

    introduced arrayexample

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // import BookList from "./assets/components/Book"; // without {} because it's a default export;
 // N:B You can change Booklist to anyname because it's a default and not named export
-import Setup from "./assets/tutorial/useState/01-example";
+// import Setup from "./assets/tutorial/useState/01-example";
+import Setup from "./assets/tutorial/useState/02-array";
 
 import "./assets/styles/bootstrap.css";
 
