64856b30478de969cef40459af918653df4467dc
kramstyles <mark.eke@decagon.dev>
2022-10-17 21:21:15 +0100

    implemented usestate

--- src/assets/tutorial/useState/01-example.js
+++ src/assets/tutorial/useState/01-example.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [title, setTitle] = useState("use state example");
+  const handleClick = () => {
+    if (title === "use state example")
+      setTitle("shake it! i feel your vibration!!");
+    else setTitle("use state example");
+  };
+
   return (
     <>
-      <div className="bg-primary p-5 text-white text-center fs-3 fw-bold">
-        Use State Error Example
+      <div className="bg-primary p-5 text-white text-center fs-3 fw-bold text-capitalize">
+        {title}
       </div>
+
+      <button
+        className="btn mt-3 btn-primary d-block m-auto"
+        onClick={handleClick}
+      >
+        Change Title
+      </button>
     </>
   );
 };
