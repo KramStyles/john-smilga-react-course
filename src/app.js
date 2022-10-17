b2e09a414ab6bbc4aa918e7f8dab24ab466a766f
kramstyles <mark.eke@decagon.dev>
2022-10-17 20:58:04 +0100

    added bootstrap

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
-import BookList from "./assets/components/Book"; // without {} because it's a default export;
+// import BookList from "./assets/components/Book"; // without {} because it's a default export;
 // N:B You can change Booklist to anyname because it's a default and not named export
+import Setup from "./assets/tutorial/useState/01-example";
+
+import "./assets/styles/bootstrap.css";
 
 const app = () => {
-  return <BookList />;
+  return <Setup />;
 };
 
 export default app;
---
+++ src/assets/tutorial/useState/01-example.js
@@ -0,0 +1,11 @@
+const Example = () => {
+  return (
+    <>
+      <div className="bg-primary p-5 text-white text-center fs-3 fw-bold">
+        Use State Error Example
+      </div>
+    </>
+  );
+};
+
+export default Example;
