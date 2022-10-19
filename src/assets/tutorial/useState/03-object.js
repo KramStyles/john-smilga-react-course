b6ae78fd394fb79851aae516d9b4ff4a507b0449
kramstyles <mark.eke@decagon.dev>
2022-10-19 13:48:38 +0100

    introduced jumbotron

--- src/assets/tutorial/useState/03-object.js
+++ src/assets/tutorial/useState/03-object.js
@@ -1,17 +1,16 @@
 import React from "react";
 
 import { profile } from "../../data/data";
+import Jumbotron from "../../components/Jumbotron";
 
 const UseStateObject = () => {
   const title = "use state object example";
   console.log(profile);
   return (
-    <>
-      <div className="bg-primary p-5 text-white text-center fs-3 fw-bold text-capitalize mb-5">
-        {title}
-      </div>
+    <React.Fragment>
+      <Jumbotron title={title} />
       <div className="container"></div>
-    </>
+    </React.Fragment>
   );
 };
 
