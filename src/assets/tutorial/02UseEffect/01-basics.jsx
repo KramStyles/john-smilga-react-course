1e67a6143f0d8a843ad120de9ed019112000b3e6
kramstyles <mark.eke@decagon.dev>
2022-10-20 15:23:36 +0100

    added useeffect.jsx

---
+++ src/assets/tutorial/02UseEffect/01-basics.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Jumbotron from '../../components/Jumbotron'
+
+const UseEffectBasics = () => {
+    const title = 'use effect basics'
+  return (
+    <>
+        <Jumbotron title={title}/>
+        <div className="container">
+
+        </div>
+    </>
+  )
+}
+
+export default UseEffectBasics
