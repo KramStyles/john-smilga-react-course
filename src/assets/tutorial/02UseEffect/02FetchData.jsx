d0dd833b84a9959694f3348354f41e370d39d6bc
kramstyles <mark.eke@decagon.dev>
2022-10-20 22:08:50 +0100

    changed table design to card

--- src/assets/tutorial/02UseEffect/02FetchData.jsx
+++ src/assets/tutorial/02UseEffect/02FetchData.jsx
@@ -21,27 +21,20 @@ const FetchData = () => {
         <Jumbotron title={title}/>
         <div className="container">
             <h2>Github Users</h2>
-            <div className="table-responsive">
-                <table className="table table-light">
-                    <thead>
-                        <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Link</th>
-                        <th>Type</th>
-                        <th>Avatar</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <td>a</td>
-                        <td>a</td>
-                        <td>a</td>
-                        <td>a</td>
-                        <td>a</td>
-                    </tr>
-                    </tbody>
-                </table>
+            <div className="row">
+                <div className="col-md-4 col-sm-12 mt-4">
+                    <div className="card" style={{width: '300px'}}>
+                        <img className="card-img-top" src="https://avatars.githubusercontent.com/u/45?v=4" alt="Card image"/>
+                        <div className="card-body">
+                            <h4 className="card-title text-primary text-capitalize">John Doe</h4>
+                            <p className="card-text border-bottom border-primary">ID:<span className='fw-bold float-end'> 1</span></p>
+                            <p className="card-text border-bottom border-primary">Node ID:<span className='fw-bold float-end'> iindie</span></p>
+                            <p className="card-text border-bottom border-primary">Type:<span className='fw-bold float-end'> user</span></p>
+                            <p className="card-text border-bottom border-primary">Site Admin:<span className='fw-bold float-end'> Yes</span></p>
+                            <a href="#" className="btn btn-dark mt-2">See Profile</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </>
