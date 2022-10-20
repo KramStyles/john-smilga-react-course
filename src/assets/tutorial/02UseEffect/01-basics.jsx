7c240343d6e7cc3c7f3f45bb2d33b344e34fd8a6
kramstyles <mark.eke@decagon.dev>
2022-10-20 16:44:47 +0100

    updated title use effect

--- src/assets/tutorial/02UseEffect/01-basics.jsx
+++ src/assets/tutorial/02UseEffect/01-basics.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 
 import Jumbotron from '../../components/Jumbotron'
 
 const UseEffectBasics = () => {
-    const title = 'use effect basics'
+    const title = 'use effect basics';
+    const [value, setValue] = useState(0)
+
+    useEffect(()=>{
+        document.title = `Messages (${value})`;
+    })
   return (
     <>
         <Jumbotron title={title}/>
         <div className="container">
-
+            <p className='text-center text-primary' style={{fontSize: '15rem'}}>{value}</p>
+            <button className="btn btn-lg btn-outline-primary m-auto d-block"
+            onClick={()=>setValue((prev)=>prev + 1)}
+            >Click to increment</button>
         </div>
     </>
   )
