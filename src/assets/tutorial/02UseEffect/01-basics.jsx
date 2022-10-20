eeac5c28e9f1ff2a54f48b22382992d75ecd2ba9
kramstyles <mark.eke@decagon.dev>
2022-10-20 17:12:20 +0100

    added dependency and window resize event listener

--- src/assets/tutorial/02UseEffect/01-basics.jsx
+++ src/assets/tutorial/02UseEffect/01-basics.jsx
@@ -4,19 +4,30 @@ import Jumbotron from '../../components/Jumbotron'
 
 const UseEffectBasics = () => {
     const title = 'use effect basics';
+    const [size, setSize] = useState(window.innerWidth)
     const [value, setValue] = useState(0)
 
     useEffect(()=>{
-        document.title = `Messages (${value})`;
-    })
+        if (value > 1) {
+            document.title = `Messages (${value})`;
+        }
+    }, [value]) // Add empty list dependency if you want the effect to run only on initial render
+    // You can always add as many use effects as possible
+
+    useEffect(() => {
+        window.addEventListener('resize', ()=>{
+            setSize(window.innerWidth);
+        }) 
+    }, [size])
   return (
     <>
         <Jumbotron title={title}/>
         <div className="container">
-            <p className='text-center text-primary' style={{fontSize: '15rem'}}>{value}</p>
-            <button className="btn btn-lg btn-outline-primary m-auto d-block"
+            <p className='text-center text-primary fs-bg'>{value}</p>
+            <button className="btn btn-lg btn-outline-primary m-auto d-block mt-3 mb-3"
             onClick={()=>setValue((prev)=>prev + 1)}
-            >Click to increment</button>
+            >Click to increment value and window title</button>
+            <p className='text-center fs-1'>Window Width: <span className='text-primary'>{size}px</span></p>
         </div>
     </>
   )
