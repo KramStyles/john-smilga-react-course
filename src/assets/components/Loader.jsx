f4778c14fee0c17ca067504ee9fe03c84f181982
kramstyles <mark.eke@decagon.dev>
2022-10-21 19:56:38 +0100

    introduced github input element

--- src/assets/components/Loader.jsx
+++ src/assets/components/Loader.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import "./../styles/loader.css";
 
-const Loader = () => {
+const Loader = ({error}) => {
   return (
     <div className='loader-body'>
         <div className="loader-section">
-            <div className="loader-shape"></div>
-            <div className="loader-text fs-1 fw-bold">Processing...</div>
+            {error ? <div className="loader-text fs-3 fw-bold text-warning">{error}</div>:
+            <>
+                <div className="loader-shape"></div>
+                <div className="loader-text fs-1 fw-bold">Processing...</div>
+            </>    
+        }
         </div>
     </div>
   )
--- src/assets/tutorial/02UseEffect/03FetchWithLoader.jsx
+++ src/assets/tutorial/02UseEffect/03FetchWithLoader.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 
 import Jumbotron from "../../components/Jumbotron";
 import Loader from '../../components/Loader';
 
 const FetchWithLoader = () => {
+    const [username, setUsername] = useState('kramstyles');
+    const url = `https://api.github.com/users/${username}`;
+
+    const getProfile = (e) => {
+        if(e.keyCode === 13){
+            setUsername(e.target.value);
+            console.log(username);
+            e.target.value = '';
+        }
+    }
   return (
     <>
         <Jumbotron title="fetch data with Loader"/>
-        <Loader/>
+        {/* <Loader error="This is wrong"/> */}
         <div className="container">
+            <div className="row">
+                <div className="col-md-6 col-sm-12">
+                    <input type="text" className='form-control' onKeyDown={getProfile} placeholder='Enter Github username to get profile'/>
+                </div>
+                <div className="col-md-6 col-sm-12">
+                    
+                </div>
+            </div>
         </div>
     </>
   )
