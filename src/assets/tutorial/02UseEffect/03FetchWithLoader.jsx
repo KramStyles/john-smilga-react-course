ce2d83387d2f81f39d90deba6b6cb725a0ba4275
kramstyles <mark.eke@decagon.dev>
2022-10-21 20:29:49 +0100

    getting response

--- src/assets/tutorial/02UseEffect/03FetchWithLoader.jsx
+++ src/assets/tutorial/02UseEffect/03FetchWithLoader.jsx
@@ -5,26 +5,43 @@ import Loader from '../../components/Loader';
 
 const FetchWithLoader = () => {
     const [username, setUsername] = useState('kramstyles');
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [errorMsg, setErrorMsg] = useState('Something Wrong');
     const url = `https://api.github.com/users/${username}`;
 
     const getProfile = (e) => {
         if(e.keyCode === 13){
             setUsername(e.target.value);
-            console.log(username);
             e.target.value = '';
         }
     }
+
+    useEffect(() => {
+        setIsLoading(true);
+      fetch(url)
+      .then((response) => response.json())
+      .then((user) => {
+          setIsLoading(false);
+          console.log(user);
+      })
+      .catch((error) => console.log('error'));
+    
+    }, [username])
+    
   return (
     <>
         <Jumbotron title="fetch data with Loader"/>
-        {/* <Loader error="This is wrong"/> */}
+        {isLoading ? <Loader/>: ''}
+        {isError ? <Loader error={errorMsg}/>: ''}
+        
         <div className="container">
             <div className="row">
                 <div className="col-md-6 col-sm-12">
                     <input type="text" className='form-control' onKeyDown={getProfile} placeholder='Enter Github username to get profile'/>
                 </div>
                 <div className="col-md-6 col-sm-12">
-                    
+
                 </div>
             </div>
         </div>
