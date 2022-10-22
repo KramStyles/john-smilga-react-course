bc659fcb8a7518051d85a407c34d18d2b1b72877
kramstyles <mark.eke@decagon.dev>
2022-10-22 23:46:43 +0100

    introduced controlled inputs

--- src/assets/tutorial/02UseEffect/05Forms.jsx
+++ src/assets/tutorial/02UseEffect/05Forms.jsx
@@ -3,9 +3,12 @@ import {useState} from 'react';
 import Jumbotron from "../../components/Jumbotron";
 
 const Forms = () => {
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('form submitted');
+        console.log(name, password);
     }
     return (
         <>
@@ -16,10 +19,12 @@ const Forms = () => {
                         <form className="form" onSubmit={handleSubmit}>
                             <div className="row">
                                 <div className="col-md-6">
-                                    <input type="text" className="form-control" placeholder="Enter Name" id="name"/>
+                                    <input type="text" className="form-control" placeholder="Enter Name" id="name" value={name}
+                                           onChange={(e)=>setName(e.target.value)}/>
                                 </div>
                                 <div className="col-md-6">
-                                    <input type="password" className="form-control" placeholder="Enter password" id="pswd"/>
+                                    <input type="password" className="form-control" placeholder="Enter password" id="pswd" value={password}
+                                           onChange={(e)=> setPassword(e.target.value)} autoComplete="on"/>
                                 </div>
                                 <div className="col-md-12 mt-3 d-grid">
                                     <button className="btn btn-outline-primary btn-lg" type={"submit"}>Submit</button>
