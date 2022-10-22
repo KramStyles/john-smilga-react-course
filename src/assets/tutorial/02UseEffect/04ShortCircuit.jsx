41ca959d572b2062a58395cc4352323539fa73e8
kramstyles <mark.eke@decagon.dev>
2022-10-22 14:24:49 +0100

    implemented tenery and shortcircuit operators

--- src/assets/tutorial/02UseEffect/04ShortCircuit.jsx
+++ src/assets/tutorial/02UseEffect/04ShortCircuit.jsx
@@ -1,13 +1,25 @@
-import {useState, useEffect} from "react";
+import {useState} from "react";
 
 import Jumbotron from "../../components/Jumbotron";
 
 const ShortCircuit = () => {
+    const [text, setText] = useState('');
+    // Introducing short circuit
+    const firstValue = text || 'Hello'; // This would pick hello if text is empty
+    const secondValue = text && 'Hi'; // This would pick hi if text exist
+
+    const toggleValues=()=>{
+        if (secondValue) setText('');
+        else setText('Bye');
+    }
     return(
         <>
             <Jumbotron title="short circuit example"/>
             <div className="container">
-                <p>Hello</p>
+                <p>{firstValue}</p>
+                <p>{secondValue ? secondValue : ' '}</p>
+                <p>{secondValue || ' '}</p>
+                <button className="btn btn-outline-primary mt-2" onClick={toggleValues}>Toggle values</button>
             </div>
         </>
     )
