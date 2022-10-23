9f40ac9ad97f96228bb3cf2ad44bd348e038f409
kramstyles <mark.eke@decagon.dev>
2022-10-23 22:43:21 +0100

    introduced age

--- src/assets/tutorial/02UseEffect/05Forms.jsx
+++ src/assets/tutorial/02UseEffect/05Forms.jsx
@@ -1,77 +1,112 @@
-import {useState} from 'react';
+import { useState } from "react";
 
 import Jumbotron from "../../components/Jumbotron";
 
 const Forms = () => {
-    const [name, setName] = useState('');
-    const [password, setPassword] = useState('');
-    const [db, setDb] = useState([]);
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [age, setAge] = useState("");
+  const [db, setDb] = useState([]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if(name && password){
-            setDb((prev)=>{
-                const createdAt = new Date().getTime().toString();
-                return [...db, [name, password, createdAt]]
-            })
-            setName('');
-            setPassword('');
-        } else alert('Please ensure all fields are complete')
-    }
-    return (
-        <>
-            <Jumbotron title="Form Examples" />
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-10 offset-md-1">
-                        <form className="form" onSubmit={handleSubmit}>
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <input type="text" className="form-control" placeholder="Enter Name" id="name" value={name}
-                                           onChange={(e)=>setName(e.target.value)}/>
-                                </div>
-                                <div className="col-md-6">
-                                    <input type="password" className="form-control" placeholder="Enter password" id="pswd" value={password}
-                                           onChange={(e)=> setPassword(e.target.value)} autoComplete="on"/>
-                                </div>
-                                <div className="col-md-12 mt-3 d-grid">
-                                    <button className="btn btn-outline-primary btn-lg" type={"submit"}>Submit</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name && password) {
+      setDb((prev) => {
+        const createdAt = new Date().getTime().toString();
+        return [...db, [name, password, age, createdAt]];
+      });
+      setName("");
+      setPassword("");
+    } else alert("Please ensure all fields are complete");
+  };
+  return (
+    <>
+      <Jumbotron title="Form Examples" />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-10 offset-md-1">
+            <form className="form" onSubmit={handleSubmit}>
+              <div className="row">
+                <div className="col-md-4">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Name"
+                    id="name"
+                    name="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
                 </div>
-                <div className="row">
-                    <div className="col-md-12">
-                        <div className="table mt-5">
-                            <table className="table table-light table-hover">
-                                <thead>
-                                    <tr>
-                                        <th>Fullname</th>
-                                        <th>Password</th>
-                                        <th>Time ID</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                {db.length > 0 ?
-                                    db.map(detail =>{
-                                       return (
-                                           <tr key={detail[2]}>
-                                               <td>{detail[0]}</td>
-                                               <td>{detail[1]}</td>
-                                               <td>{detail[2]}</td>
-                                           </tr>
-                                       )
-                                    }) : ''
-                                }
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                <div className="col-md-4">
+                  <input
+                    type="number"
+                    className="form-control"
+                    placeholder="Age"
+                    id="age"
+                    name="age"
+                    value={age}
+                    onChange={(e) => setAge(e.target.value)}
+                    autoComplete="on"
+                  />
                 </div>
+                <div className="col-md-4">
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Enter password"
+                    id="pswd"
+                    name="pswd"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    autoComplete="on"
+                  />
+                </div>
+                <div className="col-md-12 mt-3 d-grid">
+                  <button
+                    className="btn btn-outline-primary btn-lg"
+                    type={"submit"}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="table mt-5">
+              <table className="table table-light table-hover">
+                <thead>
+                  <tr>
+                    <th>Fullname</th>
+                    <th>Password</th>
+                    <th>Age</th>
+                    <th>Time ID</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {db.length > 0
+                    ? db.map((detail) => {
+                        return (
+                          <tr key={detail[2]}>
+                            <td>{detail[0]}</td>
+                            <td>{detail[1]}</td>
+                            <td>{detail[2]}</td>
+                            <td>{detail[3]}</td>
+                          </tr>
+                        );
+                      })
+                    : ""}
+                </tbody>
+              </table>
             </div>
-        </>
-    )
-}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Forms;
