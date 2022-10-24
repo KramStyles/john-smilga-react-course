ba74b16976007381171640a1d1af24d3f5d43c2d
kramstyles <mark.eke@decagon.dev>
2022-10-24 23:18:58 +0100

    implementing useref. Madoo

--- src/assets/tutorial/03useRef/01UseRef.jsx
+++ src/assets/tutorial/03useRef/01UseRef.jsx
@@ -1,12 +1,37 @@
 import { useRef } from "react";
 
+/* 
+    UseRef is similar to useState. It preserves value but unlike useState, it doesn't trigger re-render and 
+    it targets DOM node/elements
+*/
+
 import Jumbotron from "../../components/Jumbotron";
 
 const UseRefExample = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(refContainer.current.value);
+    refContainer.current.value = "";
+  };
+  const refContainer = useRef(null);
   return (
     <>
       <Jumbotron title="use ref example" />
-      <div className="container"></div>
+      <div className="container">
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter input"
+              ref={refContainer}
+            />
+          </div>
+          <div className="form-group d-grid mt-3">
+            <button className="btn btn-outline-primary">Submit</button>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
