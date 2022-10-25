42fc1fae13254373296ab4f61b0171b89d0f9962
kramstyles <mark.eke@decagon.dev>
2022-10-25 19:25:21 +0100

    set up working usestate list

--- src/assets/tutorial/04useReducer/index.jsx
+++ src/assets/tutorial/04useReducer/index.jsx
@@ -1,7 +1,7 @@
 import { useReducer, useState } from "react";
 
 import Jumbotron from "../../components/Jumbotron";
-import { profile } from "../../data/data";
+import { people } from "../../data/data";
 import Modal from "../../components/modal";
 
 // A simple usestate would go well with a simple todo list but use reducer is for more
@@ -11,23 +11,29 @@ const UseReducerBasics = () => {
   const [modalShow, setModalShow] = useState(false);
   const [modalText, setModalText] = useState("");
   const [value, setValue] = useState("");
+  const [db, setDb] = useState(people);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setModalText(value);
-    setModalShow(true);
-    setValue("");
+    if (value) {
+      setModalText(value + " added!");
+      setModalShow(true);
+      setValue("");
 
-    setTimeout(() => {
-      setModalShow(false);
-    }, 2000);
+      const data = { id: new Date().getTime().toString(), value: value };
+      setDb([...db, data]);
+
+      setTimeout(() => {
+        setModalShow(false);
+      }, 2000);
+    }
   };
   return (
     <>
       <Jumbotron title="use-reducer basics" />
       <div className="container">
         <Modal text={modalText} show={modalShow} />
-        <form className="form" onSubmit={handleSubmit}>
+        <form className="form mb-3" onSubmit={handleSubmit}>
           <div className="form-group">
             <input
               type="text"
@@ -41,6 +47,38 @@ const UseReducerBasics = () => {
             <button className="btn btn-outline-primary">Create</button>
           </div>
         </form>
+        <div className="table-responsive">
+          <table className="table table-light table-hover">
+            <thead>
+              <tr>
+                <th>S/N</th>
+                <th>Value</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {db.length > 0 ? (
+                db.map((item, index) => {
+                  return (
+                    <tr key={item.id}>
+                      <td>{index + 1}</td>
+                      <td>{item.value}</td>
+                      <td>
+                        <button className="btn btn-outline-dark btn-sm">
+                          Remove
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })
+              ) : (
+                <tr className="text-center">
+                  <td colSpan="3">Nothing to display</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
