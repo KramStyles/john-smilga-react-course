4628e720fc96242211a43cd14d568ea66e8881d1
kramstyles <mark.eke@decagon.dev>
2022-10-25 16:04:43 +0100

    showing modal on click

--- src/assets/tutorial/04useReducer/index.jsx
+++ src/assets/tutorial/04useReducer/index.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 
 import Jumbotron from "../../components/Jumbotron";
 import { profile } from "../../data/data";
@@ -8,11 +8,39 @@ import Modal from "../../components/modal";
 // complicated structures. Use reducer relies heavily on redux
 
 const UseReducerBasics = () => {
+  const [modalShow, setModalShow] = useState(false);
+  const [modalText, setModalText] = useState("");
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setModalText(value);
+    setModalShow(true);
+    setValue("");
+
+    setTimeout(() => {
+      setModalShow(false);
+    }, 2000);
+  };
   return (
     <>
       <Jumbotron title="use-reducer basics" />
       <div className="container">
-        <Modal />
+        <Modal text={modalText} show={modalShow} />
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Something!"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
+          </div>
+          <div className="d-grid mt-2">
+            <button className="btn btn-outline-primary">Create</button>
+          </div>
+        </form>
       </div>
     </>
   );
