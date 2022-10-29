b773be860e633fb3c7e8df9d484a3be0e7cffb3d
kramstyles <mark.eke@decagon.dev>
2022-10-29 13:06:16 +0100

    moved reducer function to reducer.jsx

--- src/assets/tutorial/04useReducer/index.jsx
+++ src/assets/tutorial/04useReducer/index.jsx
@@ -3,43 +3,10 @@ import { useReducer, useState } from "react";
 import Jumbotron from "../../components/Jumbotron";
 import Modal from "../../components/modal";
 import { people } from "../../data/data";
+import { reducer } from "./reducer";
 
 // A simple usestate would go well with a simple todo list but use reducer is for more
 // complicated structures. Use reducer relies heavily on redux
-
-const reducer = (state, action) => {
-  if (action.type === "ADD_ITEM") {
-    const details = [...state.db, action.payload];
-    console.log(details);
-    return {
-      ...state,
-      db: details,
-      modalShow: true,
-      modalText: action.payload.value + " added!",
-    };
-  } else if (action.type === "NO_VALUE")
-    return {
-      ...state,
-      modalShow: true,
-      modalText: "Please enter a value",
-    };
-  else if (action.type === "REMOVE_ITEM") {
-    const index = action.payload.index;
-    const item = state.db[index].value;
-    const id = state.db[index].id;
-    return {
-      ...state,
-      db: state.db.filter((person) => person.id !== id),
-      modalText: item + " Deleted",
-      modalShow: true,
-    };
-  } else
-    return {
-      ...state,
-      modalShow: false,
-      modalText: "Bye",
-    };
-};
 
 const defaultState = {
   db: people,
