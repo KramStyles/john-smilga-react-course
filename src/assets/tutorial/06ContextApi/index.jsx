719df2fb1abbd6f807f2391de0d003d3305fbcd3
kramstyles <mark.eke@decagon.dev>
2022-10-30 18:26:27 +0100

    added existing code to context api

--- src/assets/tutorial/06ContextApi/index.jsx
+++ src/assets/tutorial/06ContextApi/index.jsx
@@ -1,14 +1,52 @@
-import React from "react";
-
+import { useState, useContext, createContext } from "react";
+/* Prop drilling also involves passing down function as parameters e.g you can pass down
+removePerson the same way you'd pass down id = person.id etc. This gives rise to context apis and redux
+*/
 import Jumbotron from "../../components/Jumbotron";
+import { individuals } from "../../data/data";
 
-const index = () => {
+const PersonContext = createContext();
+// In personContext we have access to two components; Provider & Consumer
+
+const Index = () => {
+  const [people, setPeople] = useState(individuals);
+
+  const List = ({ people }) => {
+    return (
+      <ul className="list-group">
+        {people.map((person, index) => {
+          return <SinglePerson key={index} {...person} />;
+        })}
+      </ul>
+    );
+  };
+
+  const removePerson = (id) => {
+    setPeople((prev) => prev.filter((person) => person.id !== id));
+  };
+
+  const SinglePerson = ({ value, id }) => {
+    return (
+      <li className="list-group-item list-group-item-action text-capitalize">
+        {value}{" "}
+        <button
+          className="btn btn-sm float-end btn-outline-dark"
+          onClick={() => removePerson(id)}
+        >
+          Delete
+        </button>
+      </li>
+    );
+  };
+
   return (
     <>
-      <Jumbotron title="context api" />
-      <div className="container"></div>
+      <Jumbotron title="context-api example" />
+      <div className="container">
+        <List people={people} />
+      </div>
     </>
   );
 };
 
-export default index;
+export default Index;
