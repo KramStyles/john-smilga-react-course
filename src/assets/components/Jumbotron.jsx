bb9f17057a8b83fc6f8ad087dd7d9aadd067cf6e
kramstyles <mark.eke@decagon.dev>
2022-11-01 21:30:25 +0100

    created navdropdown.jsx

--- src/assets/components/Jumbotron.jsx
+++ src/assets/components/Jumbotron.jsx
@@ -1,12 +1,65 @@
-import React from 'react'
+import React from "react";
 
 const Jumbotron = (props) => {
-    const {title} = props;
+  const { title } = props;
   return (
-    <div className="bg-primary p-5 text-white text-center fs-3 fw-bold text-capitalize mb-5">
+    <>
+      <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="javascript:void(0)">
+            My React Tutorial
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#mynavbar"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="mynavbar">
+            <ul class="navbar-nav me-auto">
+              {/* <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0)">
+                  Link
+                </a>
+              </li> */}
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                >
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Link
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Another link
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      A third link
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="bg-primary p-5 text-white text-center fs-3 fw-bold text-capitalize mb-5">
         {title}
-    </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Jumbotron
+export default Jumbotron;
---
+++ src/assets/components/NavDropdown.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const NavDropdown = (props) => {
+  return <div>NavDropdown</div>;
+};
+
+NavDropdown.propTypes = {};
+
+export default NavDropdown;
