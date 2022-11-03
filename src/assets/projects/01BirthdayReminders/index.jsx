b5d110ac1629030af3cd9b199dea6fb92b70ee15
kramstyles <mark.eke@decagon.dev>
2022-11-03 16:39:16 +0100

    finished birthday reminders project

--- src/assets/projects/01BirthdayReminders/index.jsx
+++ src/assets/projects/01BirthdayReminders/index.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
+
 import { birthdayDetails } from "../../data/data";
 import Jumbotron from "../../components/Jumbotron";
 import List from "./List";
 
-const index = () => {
+const Index = () => {
+  const [birthdays, setBirthdays] = useState(birthdayDetails);
   return (
     <>
       <Jumbotron title="birthday reminder project" />
@@ -11,12 +14,26 @@ const index = () => {
           <div className="col-sm-10 offset-sm-1 col-md-6 offset-md-3">
             <div className="card bg-light">
               <div className="card-body">
-                <h3 className="card-title">0 Birthdays Today</h3>
-                {birthdayDetails.map((birthday) => (
-                  <List {...birthday} key={birthday.id} />
-                ))}
-                <div className="d-grid">
-                  <button className="btn btn-lg btn-dark">Clear all</button>
+                <h3 className="card-title mb-4">
+                  {birthdays.length} Birthdays Today
+                </h3>
+                {birthdays.length > 0 &&
+                  birthdays.map((birthday) => (
+                    <List {...birthday} key={birthday.id} />
+                  ))}
+                <div className="d-flex">
+                  <button
+                    className="btn btn-lg btn-dark w-50 me-1"
+                    onClick={() => setBirthdays([])}
+                  >
+                    Clear all
+                  </button>
+                  <button
+                    className="btn btn-lg btn-dark w-50 ms-1"
+                    onClick={() => setBirthdays(birthdayDetails)}
+                  >
+                    Repopulate
+                  </button>
                 </div>
               </div>
             </div>
@@ -27,4 +44,4 @@ const index = () => {
   );
 };
 
-export default index;
+export default Index;
