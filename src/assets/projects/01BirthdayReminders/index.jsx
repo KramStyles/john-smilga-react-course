5615db7aafb7b0aca87293c56fcd8130b6925051
kramstyles <mark.eke@decagon.dev>
2022-11-03 16:27:56 +0100

    added card title and button

--- src/assets/projects/01BirthdayReminders/index.jsx
+++ src/assets/projects/01BirthdayReminders/index.jsx
@@ -11,9 +11,13 @@ const index = () => {
           <div className="col-sm-10 offset-sm-1 col-md-6 offset-md-3">
             <div className="card bg-light">
               <div className="card-body">
+                <h3 className="card-title">0 Birthdays Today</h3>
                 {birthdayDetails.map((birthday) => (
                   <List {...birthday} key={birthday.id} />
                 ))}
+                <div className="d-grid">
+                  <button className="btn btn-lg btn-dark">Clear all</button>
+                </div>
               </div>
             </div>
           </div>
