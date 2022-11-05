82a235a403566d6e13afd17b273317e7e9c3017d
kramstyles <mark.eke@decagon.dev>
2022-11-05 19:08:58 +0100

    created fetch for tours

--- src/assets/projects/02Tours/index.jsx
+++ src/assets/projects/02Tours/index.jsx
@@ -1,10 +1,45 @@
+import { useState, useEffect } from "react";
+
 import Jumbotron from "../../components/Jumbotron";
+import Loader from "../../components/Loader";
+import Tours from "./tours";
+
+const url = "https://course-api.com/react-tours-project";
 
 const Index = () => {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  const [isError, setIsError] = useState("");
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setTours(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setIsError(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, [url]);
+
   return (
     <>
       <Jumbotron title="tours project" />
-      <div className="container">tours</div>
+      {isError ? <Loader error={isError} /> : ""}
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="container">
+          <Tours />
+        </div>
+      )}
     </>
   );
 };
