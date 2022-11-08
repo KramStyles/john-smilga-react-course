03d73e8ea3bdf2962af85d40e079e2c7889a0960
kramstyles <mark.eke@decagon.dev>
2022-11-08 21:24:39 +0100

    fixed design

--- src/assets/projects/03Reviews/index.jsx
+++ src/assets/projects/03Reviews/index.jsx
@@ -1,11 +1,14 @@
 import Jumbotron from "../../components/Jumbotron";
+import Reviews from "./reviews";
 import "./reviews.css";
 
 const Index = () => {
   return (
     <>
       <Jumbotron title="Sacked 11-07! Reviews" />
-      <div className="container">Hello and fuck you</div>
+      <div className="container">
+        <Reviews />
+      </div>
     </>
   );
 };
