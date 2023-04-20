6a4ace4258d164c8ee96895f8ee43037f01a7183
kramstyles <kramstyles@outlook.com>
2023-04-20 18:56:38 +0100

    changed design of menu item

--- src/assets/projects/05MenuDisplay/MenuItem.jsx
+++ src/assets/projects/05MenuDisplay/MenuItem.jsx
@@ -6,13 +6,15 @@ const MenuItem = () => {
     return menuItem.map((item) => {
         const {category, desc, id, img, price, title} = item;
         return (
-            <div className="col-md-4" key={id}>
-                <div className="card m-1">
-                    <img className="card-img-top" src={img} alt={title}/>
-                    <div className="card-body">
-                        <h4 className="card-title text-capitalize">{title}</h4>
-                        <p className="card-text">{desc}</p>
-                        <button className="btn btn-primary">{category} <small>${price}</small></button>
+            <div className="col-md-6" key={id}>
+                <div className="row p-3">
+                    <div className="col-md-4">
+                        <img src={img} alt={title} className="rounded img-thumbnail" width="300px"/>
+                    </div>
+                    <div className="col-md-8 d-flex flex-column justify-content-between">
+                        <h3 className="text-capitalize">{title} <span className="text-primary">${price}</span></h3>
+                        <p className="text-justify">{desc}</p>
+                        <small className="text-muted text-capitalize btn btn-sm btn-outline-dark mb-1">{category}</small>
                     </div>
                 </div>
             </div>
