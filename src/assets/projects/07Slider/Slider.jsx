d808df73e4450be4838dbc7ddc57e73f3d7c58d3
kramstyles <kramstyles@outlook.com>
2023-04-23 14:59:32 +0100

    adjusted boostrap and fixed slider functions

--- src/assets/projects/07Slider/Slider.jsx
+++ src/assets/projects/07Slider/Slider.jsx
@@ -13,17 +13,18 @@ const Slider = () => {
   return (
     <div className="container position-relative">
       <div className="row">
-        <div
-          className="position-absolute cur-pointer left-icon"
-        >
-          <CgPushChevronLeftR size={55} className="text-primary" />
+        <div className="position-absolute cur-pointer left-icon" >
+          <CgPushChevronLeftR size={55} className="text-primary" onClick={() => setIndex(index - 1)}/>
         </div>
         <div className="col-12">
           {people.map((person, personIndex) => {
             const { image, name, id, job, text } = person;
+            let position = "next-slider";
+            if (personIndex === index) position = "active-slider";
+            if (personIndex === index - 1 || (index === 0 && personIndex === people.length - 1) ) position = "last-slider";
             return (
               <div
-                className="card position-absolute shadow p-3 my-card-slider"
+                className={`card position-absolute shadow p-3 my-card-slider ${position}`}
                 key={personIndex}
               >
                 <div className="card-img-top text-center p-3">
@@ -47,16 +48,15 @@ const Slider = () => {
                     <p className="fs-5 mt-5 border-start border-5 border-primary">
                       {text}
                     </p>
-                      <CgQuoteO size={67} className="text-center text-primary"/>
+                    <CgQuoteO size={67} className="text-center text-primary" />
                   </div>
                 </div>
               </div>
             );
           })}
         </div>
-        <div className="position-absolute cur-pointer right-icon"
-        >
-          <CgPushChevronRightR size={55} className="text-primary" />
+        <div className="position-absolute cur-pointer right-icon">
+          <CgPushChevronRightR size={55} className="text-primary"  onClick={() => setIndex(index + 1)}/>
         </div>
       </div>
     </div>
