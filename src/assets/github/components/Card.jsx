91498301b262e1664fe87198a666ce561c92cbac
kramstyles <kramstyles@outlook.com>
2023-05-10 23:59:55 +0100

    introduced tab

--- src/assets/github/components/Card.jsx
+++ src/assets/github/components/Card.jsx
@@ -4,6 +4,7 @@
  */
 
 import { useGithubContext } from "../context/context";
+import Tab from "./Tab";
 const Card = () => {
   const { gitUser } = useGithubContext();
   const {
@@ -20,9 +21,14 @@ const Card = () => {
     created_at,
   } = gitUser;
   return (
-    <div className="container">
-      <h1>Card</h1>
-    </div>
+    <Tab title="Home">
+      <div className="">
+        <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro
+          synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher
+          retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip
+          placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.</p>
+      </div>
+    </Tab>
   );
 };
 
