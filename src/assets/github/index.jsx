8af7a5385120168e88f30d94953f38cf815fbe8c
kramstyles <kramstyles@outlook.com>
2023-05-17 23:48:16 +0100

    created skeleton for auth

--- src/assets/github/index.jsx
+++ src/assets/github/index.jsx
@@ -1,12 +1,22 @@
 import Jumbotron from "../components/Jumbotron";
 import Dashboard from "./pages/Dashboard";
 import { GithubProvider } from "./context/context";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+// Publickey
+// Public secret key 
 
 const Index = () => (
-  <GithubProvider>
-    <Jumbotron title="Github Project" noGap />
-    <Dashboard />
-  </GithubProvider>
+  <Auth0Provider
+  domain = "Your_Domain"
+  clientId = "Client_ID"
+  redirectUri = {window.location.origin}
+  >
+    <GithubProvider>
+      <Jumbotron title="Github Project" noGap />
+      <Dashboard />
+    </GithubProvider>
+  </Auth0Provider>
 );
 
 export default Index;
