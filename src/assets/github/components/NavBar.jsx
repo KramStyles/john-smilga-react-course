ce40014e318cf176e84eaba398b81d59e5721087
kramstyles <kramstyles@outlook.com>
2023-05-20 18:42:45 +0100

    omoo. Auth don dey misbehave

--- src/assets/github/components/NavBar.jsx
+++ src/assets/github/components/NavBar.jsx
@@ -4,27 +4,64 @@
  */
 
 import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from "react";
 
 const NavBar = () => {
   const { isAuthenticated, loginWithRedirect, logout, user, isLoading } =
     useAuth0();
+  useEffect(() => {
+    console.log({
+      isAuthenticated,
+      loginWithRedirect,
+      logout,
+      user,
+      isLoading,
+    });
+  }, []);
   return (
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-          <div className="container-fluid">
-              <a className="navbar-brand" href="javascript:void(0)">Github with Auth</a>
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#authnavbar">
-                  <span className="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="javascript:void(0)">
+          Github with Auth
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#authnavbar"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="authnavbar">
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="javascript:void(0)">
+                Link
+              </a>
+            </li>
+          </ul>
+          {isAuthenticated ? (
+            <>
+              <button
+                className="btn btn-primary"
+                type="button"
+                onClick={() => logout({ returnTo: window.location.origin })}
+              >
+                Logout
               </button>
-              <div className="collapse navbar-collapse" id="authnavbar">
-                  <ul className="navbar-nav me-auto">
-                      <li className="nav-item">
-                          <a className="nav-link" href="javascript:void(0)">Link</a>
-                      </li>
-                  </ul>
-                  <button className="btn btn-primary" type="button" onClick={loginWithRedirect}>Login</button>
-              </div>
-          </div>
-      </nav>
+            </>
+          ) : (
+            <button
+              className="btn btn-primary"
+              type="button"
+              onClick={loginWithRedirect}
+            >
+              Login
+            </button>
+          )}
+        </div>
+      </div>
+    </nav>
   );
 };
 
