acc2659de5d54b36961c5c5584e4556fa9ea8788
kramstyles <kramstyles@outlook.com>
2024-04-05 20:14:06 +0100

    haven't added other pages but fixed activation today

--- src/assets/ecommerce/components/molecules/NavBar/index.jsx
+++ src/assets/ecommerce/components/molecules/NavBar/index.jsx
@@ -1,5 +1,21 @@
+import styled from "styled-components";
+import Logo from "../../images/logo.png";
+import { FaBars } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const NavContainer = styled.nav`
+    height: 5rem;
+
+    .nav-center {
+        width: 90vw;
+        margin: 0 auto;
+    }
+`
+
 const Index = () => {
-    return <h3>NavBar</h3>
+    return <NavContainer className="d-flex align-items-center justify-content-center">
+
+    </NavContainer>
 }
 
 export default Index;
